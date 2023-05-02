cef446928c68c2592b60dbcd02cd71dc25533dac
Strikeeaglechase <Strikeeaglechase@gmail.com>
2023-05-02 15:50:26 -0400

    Added ban command

---
+++ src/commands/admin/ban.ts
@@ -0,0 +1,26 @@
+import Discord from "discord.js";
+import { Arg, CommandRun } from "strike-discord-framework/dist/argumentParser.js";
+import { Command, CommandEvent } from "strike-discord-framework/dist/command.js";
+
+import { Application } from "../../application.js";
+
+class Ban extends Command {
+	name = "ban";
+	allowDm = false;
+	help = {
+		msg: "Bans a user",
+	};
+
+	@CommandRun
+	async run({ message, framework, app }: CommandEvent<Application>, @Arg({}) userId: string) {
+		let user = await app.users.get(userId);
+		if (!user) user = await app.createNewUser(userId);
+
+		user.isBanned = true;
+		await app.users.update(user, user.id);
+
+		return framework.success(`Banned ${user.pilotNames[0] ?? user.id}`);
+	}
+}
+
+export default Ban;
